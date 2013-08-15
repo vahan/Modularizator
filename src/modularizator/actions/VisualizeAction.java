f9f8073b70a570e9afe5d5110c8d2f48dc573b48
Vahan <vahanh@student.ethz.ch>
2013-08-15 11:57:15 +0200

    visualized with gephi

--- src/modularizator/actions/VisualizeAction.java
+++ src/modularizator/actions/VisualizeAction.java
@@ -1,14 +1,22 @@
 package modularizator.actions;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Random;
 
+import javax.swing.JFrame;
+
 import logic.Cluster;
 import logic.Network;
 
 import org.apache.commons.lang3.StringUtils;
+import org.eclipse.gef4.zest.core.widgets.Graph;
 import org.eclipse.gef4.zest.dot.DotGraph;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jface.action.IAction;
@@ -17,7 +25,33 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.gephi.data.attributes.api.AttributeController;
+import org.gephi.data.attributes.api.AttributeModel;
+import org.gephi.graph.api.DirectedGraph;
+import org.gephi.graph.api.Edge;
+import org.gephi.graph.api.GraphController;
+import org.gephi.graph.api.GraphModel;
+import org.gephi.graph.api.Node;
+import org.gephi.io.importer.api.Container;
+import org.gephi.io.importer.api.ImportController;
+import org.gephi.layout.plugin.force.StepDisplacement;
+import org.gephi.layout.plugin.force.yifanHu.YifanHuLayout;
+import org.gephi.partition.api.Partition;
+import org.gephi.partition.api.PartitionController;
+import org.gephi.partition.plugin.NodeColorTransformer;
+import org.gephi.preview.api.PreviewController;
+import org.gephi.preview.api.PreviewModel;
+import org.gephi.preview.api.PreviewProperties;
+import org.gephi.preview.api.PreviewProperty;
+import org.gephi.preview.api.ProcessingTarget;
+import org.gephi.preview.api.RenderTarget;
+import org.gephi.preview.types.DependantOriginalColor;
+import org.gephi.project.api.ProjectController;
+import org.gephi.project.api.Workspace;
 import org.jgrapht.graph.DefaultEdge;
+import org.openide.util.Lookup;
+
+import processing.core.PApplet;
 
 
 public class VisualizeAction extends BaseAction {
@@ -30,7 +64,9 @@ public class VisualizeAction extends BaseAction {
 	public void run(IAction action) {
 		network = readNetwork();
 		
-		show();
+		makeGephi();
+		
+		//show();
 		
 		/*GraphWindow window = new GraphWindow(network);
 		Thread thread = new Thread(window);
@@ -42,10 +78,128 @@ public class VisualizeAction extends BaseAction {
 			e.printStackTrace();
 		}*/
 	}
+	
+	private void makeGephi() {
+		// Init a project - and therefore a workspace
+		ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
+		pc.newProject();
+		Workspace workspace = pc.getCurrentWorkspace();
+		GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getModel();
+		DirectedGraph dGraph = graphModel.getDirectedGraph();
+		//Represent the network via clusters
+		HashMap<ICompilationUnit, Cluster> nodes = network.getClusters();
+		//Add vertices to the graph
+		for (Entry<ICompilationUnit, Cluster> entry : nodes.entrySet()) {
+			ICompilationUnit cu = entry.getKey();
+			Cluster cluster = entry.getValue();
+			Node node = graphModel.factory().newNode(vertexName(cu));
+			node.getNodeData().setLabel(Integer.toString(cluster.getId()));
+			dGraph.addNode(node);
+		}
+		//Add edges to graph
+		for (DefaultEdge e : network.edgeSet()) {
+			ICompilationUnit source = network.getEdgeSource(e);
+			Node sourceNode = dGraph.getNode(vertexName(source));
+			ICompilationUnit target = network.getEdgeTarget(e);
+			Node targetNode = dGraph.getNode(vertexName(target));
+			Edge edge = graphModel.factory().newEdge(sourceNode, targetNode, 1f, true);
+			edge.getEdgeData().setLabel(sourceNode.getNodeData().getLabel());
+			dGraph.addEdge(edge);
+		}
+		
+		// Preview configuration
+		PreviewController previewController = Lookup.getDefault().lookup(PreviewController.class);
+		PreviewModel prModel = previewController.getModel();
+		PreviewProperties props = prModel.getProperties();
+		props.putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.FALSE);
+		props.putValue(PreviewProperty.NODE_BORDER_WIDTH, 0f);
+		//props.putValue(PreviewProperty.NODE_LABEL_COLOR, new DependantOriginalColor(Color.BLACK));
+		props.putValue(PreviewProperty.BACKGROUND_COLOR, Color.WHITE);
+		previewController.refreshPreview();
+
+		//Partition the graph and color accordingly
+        PartitionController partitionController = Lookup.getDefault().lookup(PartitionController.class);
+        AttributeModel attributeModel = Lookup.getDefault().lookup(AttributeController.class).getModel();
+        Partition p = partitionController.buildPartition(attributeModel.getNodeTable().getColumn("label"), dGraph);
+        NodeColorTransformer nodeColorTransformer = new NodeColorTransformer();
+        nodeColorTransformer.randomizeColors(p);
+        partitionController.transform(p, nodeColorTransformer);
+		
+		// Run Yifan Hu layout algorithm on the graph
+		YifanHuLayout layout = new YifanHuLayout(null, new StepDisplacement(1f));
+		layout.setGraphModel(graphModel);
+		layout.resetPropertiesValues();
+		layout.setOptimalDistance(200f);
+		layout.initAlgo();
+		for (int i = 0; i < 10000 && layout.canAlgo(); ++i) {
+			layout.goAlgo();
+		}
+		
+		//New Processing target, get the PApplet
+		ProcessingTarget target = (ProcessingTarget) previewController.getRenderTarget(RenderTarget.PROCESSING_TARGET);
+		PApplet applet = target.getApplet();
+		applet.init();
+
+		//Refresh the preview and reset the zoom
+		previewController.render(target);
+		target.refresh();
+		target.resetZoom();
+
+		//Add the applet to a JFrame and display
+		JFrame frame = new JFrame("Interaction Graph");
+		frame.setLayout(new BorderLayout());
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(applet, BorderLayout.CENTER);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	
+	private void showGephi(DirectedGraph dGraph) {
+		/*GraphModel graphModel = dGraph.getGraphModel();
+		YifanHuLayout layout = new YifanHuLayout(null, new StepDisplacement(1f));
+		layout.setGraphModel(graphModel);
+		layout.resetPropertiesValues();
+		layout.setOptimalDistance(200f);
+		layout.initAlgo();
+		for (int i = 0; i < 100 && layout.canAlgo(); ++i) {
+			layout.goAlgo();
+		}*/
+		
+		// Preview configuration
+		PreviewController previewController = Lookup.getDefault().lookup(PreviewController.class);
+		PreviewModel prModel = previewController.getModel();
+		PreviewProperties props = prModel.getProperties();
+		props.putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.TRUE);
+		previewController.refreshPreview();
+		
+		//New Processing target, get the PApplet
+		ProcessingTarget target = (ProcessingTarget) previewController.getRenderTarget(RenderTarget.PROCESSING_TARGET);
+		PApplet applet = target.getApplet();
+		applet.init();
+
+		//Refresh the preview and reset the zoom
+		previewController.render(target);
+		target.refresh();
+		target.resetZoom();
+
+		//Add the applet to a JFrame and display
+		JFrame frame = new JFrame("Interaction Graph");
+		frame.setLayout(new BorderLayout());
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(applet, BorderLayout.CENTER);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
 
 	private void show() {
 		//Represent the network via clusters
-		HashMap<Cluster, ArrayList<ICompilationUnit>> clusters = new HashMap<Cluster, ArrayList<ICompilationUnit>>();
+		/*HashMap<Cluster, ArrayList<ICompilationUnit>> clusters = new HashMap<Cluster, ArrayList<ICompilationUnit>>();
 		for (Entry<ICompilationUnit, Cluster> entry : network.getClusters().entrySet()) {
 			ArrayList<ICompilationUnit> compUnits = clusters.get(entry.getValue());
 			if (compUnits == null) {
@@ -58,14 +212,14 @@ public class VisualizeAction extends BaseAction {
 		}
 		//Make a dot string
 		//Add vertices to the dot string
-		String dotStr = "digraph NAME { node [shape=point]; ";
+		String dotStr = "digraph NAME { node [label=\"\", shape=point, style=filled, fillcolor=red]; ";
 		for (Entry<Cluster, ArrayList<ICompilationUnit>> entry : clusters.entrySet()) {
 			String clusterName = entry.getKey().getModel().getElementName();
-			dotStr += "subgraph cluster_" + format(clusterName) + " { ";
-			
-			dotStr += "node [label=\"\",style=filled,color=\"" + randomColor() + "\"]; ";
+			dotStr += "subgraph cl_" + format(clusterName) + " { rank = same; ";
+			String color = randomColor();
+			//dotStr += "node [fillcolor=red, color=\"" + color + "\"]; ";
 			for (ICompilationUnit compUnit : entry.getValue()) {
-				dotStr += vertexName(compUnit) + "; ";
+				dotStr += vertexName(compUnit) + " [fillcolor=green, color=\"" + color + "\"]; ";
 			}
 			dotStr += " } ";
 		}
@@ -76,7 +230,24 @@ public class VisualizeAction extends BaseAction {
 			dotStr += " " + vertexName(source) + " -> " + vertexName(target) + "; ";
 		}
 		
-		dotStr += "} ";
+		dotStr += "} ";*/
+		BufferedReader br = null;
+		String dotStr = "";
+		try {
+ 			String sCurrentLine;
+ 			br = new BufferedReader(new FileReader("/home/vahan/Desktop/test.dot"));
+ 			while ((sCurrentLine = br.readLine()) != null) {
+				dotStr += sCurrentLine + "\n";
+			}
+ 		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 		
 		MessageDialog.openInformation(shell, "dotStr", dotStr);
 		
@@ -85,9 +256,8 @@ public class VisualizeAction extends BaseAction {
 		DotGraph graph = new DotGraph(dotStr, shell, SWT.NONE);
 		//DotGraph graph = new DotGraph("digraph { subgraph cluster1 { n1; } subgraph cluster2 { n2; } subgraph cluster3 { n3; } n1 -> n2; n2 -> n3; n3 -> n1; }", shell, SWT.NONE);
 		
-		shell.setText(DotGraph.class.getSimpleName());
-		shell.setLayout(new FillLayout());
-		shell.setSize(600, 300);
+		//graph.pack();
+		//shell.pack();
 		shell.open();
 		Display display = shell.getDisplay();
 		while (!shell.isDisposed())
@@ -95,6 +265,7 @@ public class VisualizeAction extends BaseAction {
 				display.sleep();
 		display.dispose();
 	}
+	
 	
 	private String vertexName(ICompilationUnit vertex) {
 		Cluster cluster = network.getCluster(vertex);
